fa9b646532fef814e39887e7a3d2df75fcfbd1d5
abhaykss <abhayksaxena19@gmail.com>
2026-02-25 04:58:27 +0530

    Added dynamic PORT for deployment

--- server.js
+++ server.js
@@ -84,6 +84,8 @@ app.get("/:code", async (req, res) => {
 });
 
 // 🔹 Start Server
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
